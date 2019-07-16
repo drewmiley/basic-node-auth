6f4bd2ca8fef4864be251a219595893417d4add5
Drew Miley <drewmiley92@gmail.com>
2019-07-16 17:41:17 +0100

    Unit test for missing authorization header

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,9 +1,19 @@
-var expect  = require('chai').expect;
-var request = require('request');
+const expect  = require('chai').expect;
+const request = require('request');
+const sinon = require('sinon');
+
+const authentication = require('../src/authentication');
+
+const res = { status: status => ({ json: json => ({ status, json }) })};
+const next = () => 'next';
 
 describe('Authentication', () => {
     it('Should return missing authorization header', done => {
-        expect(false).to.be.true;
+        const req = { headers: { authorization: null } };
+        authentication(req, res, next).then(res => {
+            expect(res.status).to.equal(401);
+            expect(res.json).to.equal('Missing Authorization Header');
+        });
         done();
     });
     it('Should return invalid auth credentials for invalid user authentication', done => {
